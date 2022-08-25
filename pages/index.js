9fe43bc9dc7a00a8140f55fd07c821f40498ae70
JuanIgnacioMarchetto <juanignaciomarchetto@gmail.com>
2022-08-25 09:23:05 -0300

    home

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,90 @@
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold">
-      Wallet
-    </h1>
-  )
+    <div className="bg-black pb-72 pt-6 place-content-evenly grid  ">
+      <div className=" container mx-auto px-5place-content-evenly grid grid-cols-3 justify-center items-center ">
+      <div className=" mx-2 my-2  ">
+        <a   href="./pag2.html">
+          <img rounded-full className="rounded-full  w-28"
+            src="https://rickandmortyapi.com/api/character/avatar/18.jpeg"
+            alt=""
+          />
+        </a>
+      </div>
+      <div className=" mx-2 my-2  ">
+        <a   href="./pag2.html">
+          <img rounded-full className="rounded-full h-28 w-28"
+            src="https://rickandmortyapi.com/api/character/avatar/18.jpeg"
+            alt=""
+          />
+        </a>
+      </div>
+      <div className=" mx-2 my-2  ">
+        <a   href="./pag2.html">
+          <img rounded-full className="rounded-full h-28 w-28"
+            src="https://rickandmortyapi.com/api/character/avatar/18.jpeg"
+            alt=""
+          />
+        </a>
+      </div>
+      <div className=" mx-2 my-2">
+        <a   href="./pag2.html">
+          <img rounded-full className="rounded-full h-28 w-28"
+            src="https://rickandmortyapi.com/api/character/avatar/18.jpeg"
+            alt=""
+          />
+        </a>
+      </div>
+      <div className=" mx-2 my-2 ">
+        <a   href="./pag2.html">
+          <img rounded-full className="rounded-full h-28 w-28"
+            src="https://rickandmortyapi.com/api/character/avatar/18.jpeg"
+            alt=""
+          />
+        </a>
+      </div>
+      <div className=" mx-2 my-2 ">
+        <a   href="./pag2.html">
+          <img rounded-full className="rounded-full h-28 w-28"
+            src="https://rickandmortyapi.com/api/character/avatar/18.jpeg"
+            alt=""
+          />
+        </a>
+      </div>
+      <div className=" mx-2 my-2">
+        <a   href="./pag2.html">
+          <img rounded-full className="rounded-full h-28 w-28"
+            src="https://rickandmortyapi.com/api/character/avatar/18.jpeg"
+            alt=""
+          />
+        </a>
+      </div>
+      <div className=" mx-2 my-2">
+        <a   href="./pag2.html">
+          <img rounded-full className="rounded-full h-28 w-28"
+            src="https://rickandmortyapi.com/api/character/avatar/18.jpeg"
+            alt=""
+          />
+        </a>
+      </div>
+      <div className=" mx-2 my-2">
+        <a   href="./pag2.html">
+          <img rounded-full className="rounded-full h-28 w-28"
+            src="https://rickandmortyapi.com/api/character/avatar/18.jpeg"
+            alt=""
+          />
+        </a>
+      </div>
+      </div>
+      <br/>
+      <div  className= "grid justify-center items-center mx-auto" >
+      <a  href="./pag2.html">
+        <img className=" h-28 w-40 "
+            src="https://image.shutterstock.com/image-vector/hamburger-menu-web-icon-white-260nw-1249309132.jpg"
+            alt=""
+          />
+      </a>
+      
+      </div>
+    </div>
+  );
 }
