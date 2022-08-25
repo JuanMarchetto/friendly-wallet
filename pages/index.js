b73c78bd78533d70c1a8b7d8245846c66a327d2b
JuanMarchetto <juanpatriciomarchetto@gmail.com>
2022-08-25 12:20:10 -0300

    Moralis

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,21 @@
-function HomePage() {
-    return <div>Welcome to Next.js!</div>;
-}
+import Head from 'next/head'
+import axios from "axios"
 
-export default HomePage;
+export default function Home() {
+
+  async function getName(){
+    const response = await axios.get('api/hello');
+    console.log(response);
+  }
+
+  return (
+    <div>
+      <Head>
+        <title>Friendly Wallet</title>
+        <meta name="description" content="Friendly Wallet" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <button onClick={getName}>Get</button>
+    </div>
+  )
+}
