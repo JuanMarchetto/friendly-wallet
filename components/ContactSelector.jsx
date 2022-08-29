580e1a03b78126f2a94de79b0371c3944badccae
JuanMarchetto <juanpatriciomarchetto@gmail.com>
2022-08-29 09:03:01 -0300

    components lecure

---
+++ components/ContactSelector.jsx
@@ -0,0 +1,14 @@
+const ContactSelector = ({img, url}) => (
+  <div className="mx-2 my-2  ">
+    <a href={url}>
+      <img
+        rounded-full
+        className="rounded-full w-28"
+        src={img}
+        alt=""
+      />
+    </a>
+  </div>
+);
+
+export default ContactSelector
--- pages/index.js
+++ pages/index.js
@@ -1,79 +1,53 @@
+import ContactSelector from "../components/ContactSelector";
+
+const contacts = [
+  {
+    img:"https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+    url:"./pag2.html"
+  },
+  {
+    img:"https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+    url:"./pag2.html"
+  },
+  {
+    img:"https://rickandmortyapi.com/api/character/avatar/3.jpeg",
+    url:"./pag2.html"
+  },
+  {
+    img:"https://rickandmortyapi.com/api/character/avatar/4.jpeg",
+    url:"./pag2.html"
+  },
+  {
+    img:"https://rickandmortyapi.com/api/character/avatar/5.jpeg",
+    url:"./pag2.html"
+  },
+  {
+    img:"https://rickandmortyapi.com/api/character/avatar/6.jpeg",
+    url:"./pag2.html"
+  },
+  {
+    img:"https://rickandmortyapi.com/api/character/avatar/18.jpeg",
+    url:"./pag2.html"
+  },
+  {
+    img:"https://rickandmortyapi.com/api/character/avatar/8.jpeg",
+    url:"./pag2.html"
+  },
+  {
+    img:"https://rickandmortyapi.com/api/character/avatar/9.jpeg",
+    url:"./pag2.html"
+  },
+];
 export default function Home() {
   return (
     <div className="bg-black pb-72 pt-6 place-content-evenly grid  ">
       <div className=" container mx-auto px-5place-content-evenly grid grid-cols-3 justify-center items-center ">
-      <div className=" mx-2 my-2  ">
-        <a   href="./pag2.html">
-          <img rounded-full className="rounded-full  w-28"
-            src="https://rickandmortyapi.com/api/character/avatar/18.jpeg"
-            alt=""
-          />
-        </a>
-      </div>
-      <div className=" mx-2 my-2  ">
-        <a   href="./pag2.html">
-          <img rounded-full className="rounded-full h-28 w-28"
-            src="https://rickandmortyapi.com/api/character/avatar/18.jpeg"
-            alt=""
-          />
-        </a>
-      </div>
-      <div className=" mx-2 my-2  ">
-        <a   href="./pag2.html">
-          <img rounded-full className="rounded-full h-28 w-28"
-            src="https://rickandmortyapi.com/api/character/avatar/18.jpeg"
-            alt=""
-          />
-        </a>
-      </div>
-      <div className=" mx-2 my-2">
-        <a   href="./pag2.html">
-          <img rounded-full className="rounded-full h-28 w-28"
-            src="https://rickandmortyapi.com/api/character/avatar/18.jpeg"
-            alt=""
-          />
-        </a>
-      </div>
-      <div className=" mx-2 my-2 ">
-        <a   href="./pag2.html">
-          <img rounded-full className="rounded-full h-28 w-28"
-            src="https://rickandmortyapi.com/api/character/avatar/18.jpeg"
-            alt=""
-          />
-        </a>
-      </div>
-      <div className=" mx-2 my-2 ">
-        <a   href="./pag2.html">
-          <img rounded-full className="rounded-full h-28 w-28"
-            src="https://rickandmortyapi.com/api/character/avatar/18.jpeg"
-            alt=""
-          />
-        </a>
-      </div>
-      <div className=" mx-2 my-2">
-        <a   href="./pag2.html">
-          <img rounded-full className="rounded-full h-28 w-28"
-            src="https://rickandmortyapi.com/api/character/avatar/18.jpeg"
-            alt=""
-          />
-        </a>
-      </div>
-      <div className=" mx-2 my-2">
-        <a   href="./pag2.html">
-          <img rounded-full className="rounded-full h-28 w-28"
-            src="https://rickandmortyapi.com/api/character/avatar/18.jpeg"
-            alt=""
-          />
-        </a>
-      </div>
-      <div className=" mx-2 my-2">
-        <a   href="./pag2.html">
-          <img rounded-full className="rounded-full h-28 w-28"
-            src="https://rickandmortyapi.com/api/character/avatar/18.jpeg"
-            alt=""
-          />
-        </a>
-      </div>
+      {contacts.map(({img, url})=>(
+        <ContactSelector
+          img={img}
+          url={url}
+        />)
+      )}
       </div>
       <br/>
       <div  className= "grid justify-center items-center mx-auto" >
